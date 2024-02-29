aaf0e29fb9b55fe1ce360c23ef697ce6bb63800e
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-29 19:32:53 +0000

    added #include main.h to _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include "main.h"
+
 // we will have to include our own header file and inside we will need our table of format specifiers and functions and prototypes
 
 _printf(const char *format, ...)
