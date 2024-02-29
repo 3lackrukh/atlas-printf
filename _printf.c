441d26bb0757bfeccdb0ca32e3151950305f4e32
root <jaylenestella24@gmail.com>
2024-02-29 17:59:08 +0000

    created _printf.c file and added our headers and prototype

---
+++ _printf.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <string.h>
+// we will have to include our own header file and inside we will need our table of format specifiers and functions and prototypes
+
+_printf(const char *format, ...)
