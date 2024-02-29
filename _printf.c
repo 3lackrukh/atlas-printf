a9014eaf5099c668cda2854330bdb86255361cbb
3lackrukh <nathan.rhys@atlasschool.com>
2024-02-29 19:23:10 +0000

    Declared variables in _printf.c, added a strlen check for cons format.

--- _printf.c
+++ _printf.c
@@ -3,3 +3,13 @@
 // we will have to include our own header file and inside we will need our table of format specifiers and functions and prototypes
 
 _printf(const char *format, ...)
+{
+	va_list santa_bag;
+	int i, L1;
+
+	va_start (santa_bag, format);
+
+	L1 = strlen(format)
+
+}
+	
