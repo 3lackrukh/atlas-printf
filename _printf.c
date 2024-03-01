f443ac33e9768d662417fabcbac0e04d9964cad0
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-01 04:28:44 +0000

    Added blank file print_spec_c.c to hold check for print specifiers. added check for '%' to _printf.c

--- _printf.c
+++ _printf.c
@@ -2,15 +2,35 @@
 #include <string.h>
 #include "main.h"
 
-// we will have to include our own header file and inside we will need our table of format specifiers and functions and prototypes
+// we will have to include our own header file and 
+// inside we will need our table of format specifiers 
+// and functions and prototypes
 
 _printf(const char *format, ...)
 {
 	va_list santa_bag;
-	int i;
+	int i, L1;
 	int char_print = 0;
+	int arg_cnt = 0;
 
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	L1 = strlen(format);
 	va_start (santa_bag, format);
 
+	while(i = 0; i < L1; i++) // iterate through string to print
+	{
+		if (format[i] != '%')
+		{
+			putchar(format[i];
+			char_print++;
+		}
+		else // % is found
+		{
+			form_spec(format[i + 1]);//send next char
+						 //to form_spec 
+						 //to check if c,s,i,d
 }
 	
