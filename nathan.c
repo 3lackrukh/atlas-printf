543b3321334b018d9005d24bb2e9f03713b07e1b
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-02 21:34:00 +0000

    op_d recieves int num. accounts for negative numbers. converts int into char string, uses string iterator to save char count. uses putchar i-- to print string in reverse, yielding integer in correct order. returns int count to add to char counter in _printf.

--- nathan.c
+++ nathan.c
@@ -2,8 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-int op_d(signed int n)
+int op_d(va_list santa_bag)
 {
-	int i;
+	int i, count;
+	int num = va_arg(santa_bag, int);
+	int sign = num < 0 ? -1 : 1;
+	char str[11];// allocates memory for max digits for int
+		     // plus 1 for the - symbol
 
-while
+	num *= sign; // if num is - value, changes to + value
+		     // for easier handling
+
+	for(i = 0; num > 0; i++)
+	{
+		str[i] = (num % 10) + '0'; //converts int to ASCII value
+		num /= 10;
+	}
+
+	if(sign == -1)
+	{
+		str[i++] = '-' //add the negative symbol to string
+	}
+
+	count = i; //saves the number of char's to be printed
+	i--;
+	while (i >= 0)
+	{
+		putchar(str[i]);//print the string in reverse
+		i--;
+	}
+	return(count); //return value added to print_arg count in _printf
+}
