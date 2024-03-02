5f1800bd01934b3925ccd945dc40f631413f9a11
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-02 22:28:54 +0000

    op_d and op_int are functionally identical. Copied op_d and renamed it op_int.

--- nathan.c
+++ nathan.c
@@ -33,3 +33,33 @@ int op_d(va_list santa_bag)
 	}
 	return(count); //return value added to print_arg count in _printf
 }
+
+int op_int(va_list santa_bag)
+{
+	int i, count;
+	int num = va_arg (santa_bag, int);
+	int sign = num < 0 ? -1 : 1;
+	char str[11];
+
+	num *= sign;
+
+	for (i = 0; num > 0; i++)
+	{
+		str[i] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	if(sign == -1)
+	{
+		str[i++] = '-'
+	}
+
+	count = i;
+	i--;
+	while (i >= 0)
+	{
+		putchar(str[i]);
+		i--;
+	}
+	return(count);
+}
