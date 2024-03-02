1ee1a9b4fa9e30c0f8930cbcea590823d45fc067
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-02 02:38:50 +0000

    added int total for return value changed first for loop comparison statement to format[i] != '\0' added missing close parenthesis to arg_print +=... returns total -1 to account for the null byte.

--- _printf.c
+++ _printf.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
 
-// we will have to include our own header file and 
-// inside we will need our table of format specifiers 
-// and function]s and prototypes
+/**
+ *
+ *
+ */
 
 _printf(const char *format, ...)
 {
 	va_list santa_bag;
 	va_start(santa_bag, format);
 
-	int i, k;
+	int i, k, total;
 	int char_print = 0;
 	int arg_print = 0;
 
@@ -48,12 +47,12 @@ _printf(const char *format, ...)
 			{
 				if (format[i] == specifier_calls[k].specifier)
 				{
-					arg_print += (specifier_calls[k].function(santa_bag);
+					arg_print += (specifier_calls[k].function(santa_bag));
 				}
 			}
 		}
-	{
+	}
 	va_end(santa_bag);
-	return(char_print + arg_print);
+	total = ((char_print + arg_print) -1);
+	return(total);
 }
-	
