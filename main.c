47f461a9145a86c5283357a253df1816f4fa3ef8
3lackrukh <nathan.rhys@atlasschool.com>
2024-03-04 14:10:25 +0000

    updated test file main.c

--- main.c
+++ main.c
@@ -12,7 +12,6 @@ int main(void)
     int len;
     int len2;
     unsigned int ui;
-    void *addr;
    
     
     _printf("%");
@@ -33,7 +32,6 @@ int main(void)
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
@@ -48,13 +46,24 @@ int main(void)
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
     len = _printf("Percent:[%%]\n");
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
+
+	_printf("%d", 0);
+	printf("%d", 0);
+	_printf("%d", INT_MIN);
+	printf("%d", INT_MIN);
+	_printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+	printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+	_printf("%i", 0);
+	printf("%i", 0);
+	_printf("%i", INT_MIN);
+	printf("%i", INT_MIN);
+	_printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
+	printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
     return (0);
 }
